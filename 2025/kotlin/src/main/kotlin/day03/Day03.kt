7aca4bbbdc3e99bde4ef4dd0dd8a7e754fc1a5c1
revzik <b.piekarz1997@gmail.com>
2025-12-04 01:23:05 +0100

    feat: solution for day3 part2

--- 2025/kotlin/src/main/kotlin/day03/Day03.kt
+++ 2025/kotlin/src/main/kotlin/day03/Day03.kt
@@ -21,23 +21,6 @@ fun part1(input: List<String>): Int {
     return result
 }
 
-fun convertBank(bank: String): List<Int> {
-    return bank.map { c -> c.digitToInt() }
-}
-
-fun findHighestFirstIdx(bank: List<Int>): Int {
-    // for bank 123456789 find the highest from 12345678,
-    // since we can't make a battery if we select the last digit as first value
-    val max = bank.subList(0, bank.size - 1).max()
-    return bank.indexOf(max)
-}
-
-fun findHighestSecond(bank: List<Int>, idxToIgnore: Int): Int {
-    // here we'll remove the elements before index from previous search and find the largest value
-    // we want to search only for numbers appearing after the previously found value
-    return bank.subList(idxToIgnore + 1, bank.size).max()
-}
-
 fun part2(input: List<String>): Long {
     var result = 0L
 
@@ -46,7 +29,57 @@ fun part2(input: List<String>): Long {
     // 2. find the biggest second index in the line that comes 11 digits between previous and last one
     // 3. find the biggest third index in the line that comes 10 digits between previous and last one
     // 4. etc.
-
+    for (strBank in input) {
+        val bank = convertBank(strBank)
+        val maxIndexes = findHighestIndexes(bank)
+        result += createCell(bank, maxIndexes)
+    }
 
     return result
+}
+
+fun convertBank(bank: String): List<Int> {
+    return bank.map { c -> c.digitToInt() }
+}
+
+fun findHighestFirstIdx(bank: List<Int>, upToIdx: Int = 1): Int {
+    // for bank 123456789 find the highest from 12345678,
+    // since we can't make a battery if we select the last digit as first value
+    val possibleBatteries = bank.subList(0, bank.size - upToIdx)
+    return bank.indexOf(possibleBatteries.max())
+}
+
+fun findHighestSecond(bank: List<Int>, idxToIgnore: Int): Int {
+    // here we'll remove the elements before index from previous search and find the largest value
+    // we want to search only for numbers appearing after the previously found value
+    return bank.subList(idxToIgnore + 1, bank.size).max()
+}
+
+fun findHighestIndexes(bank: List<Int>, amount: Int = 12): List<Int> {
+    val maxIndexes = mutableListOf(-1)
+    var workingBank = bank
+
+    for (i in 0..<amount) {
+        val maxIndex = findHighestFirstIdx(workingBank, amount - i - 1)
+        maxIndexes.addLast(maxIndex + maxIndexes.last() + 1)
+        workingBank = bank.subList(maxIndexes.last() + 1, bank.size)
+    }
+
+    return maxIndexes.subList(1, maxIndexes.size)
+}
+
+fun createCell(bank: List<Int>, maxIndexes: List<Int>): Long {
+    val batteries = maxIndexes.map { idx -> bank[idx] }
+    return consolidateBatteries(batteries)
+}
+
+// can it be rewritten into reduce ??
+fun consolidateBatteries(batteries: List<Int>): Long {
+    var power = 0L
+    var multiplier = 1L
+    for (battery in batteries.reversed()) {
+        power += multiplier * battery
+        multiplier *= 10
+    }
+    return power
 }
--- 2025/kotlin/src/test/kotlin/day03/Day03Test.kt
+++ 2025/kotlin/src/test/kotlin/day03/Day03Test.kt
@@ -18,7 +18,7 @@ class Day03Test {
 
     @Test
     fun part2Test() {
-        assertEquals(4174379265L, part2(testData1))
+        assertEquals(3121910778619, part2(testData1))
     }
 
     @Test
@@ -35,5 +35,15 @@ class Day03Test {
         assertEquals(8, findHighestSecond(listOf(9, 8, 7, 6, 5, 4, 3, 2, 1), 0))
         assertEquals(1, findHighestSecond(listOf(1, 1, 1, 1), 0))
         assertEquals(4, findHighestSecond(listOf(1, 1, 4, 4), 2))
+    }
+
+    @Test
+    fun findHighestIndexesTest() {
+        assertEquals(listOf(0, 2, 4), findHighestIndexes(listOf(9, 5, 9, 4, 8), 3))
+    }
+
+    @Test
+    fun createCellTest() {
+        assertEquals(434234234278, createCell(convertBank("234234234234278"), listOf(2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)))
     }
 }
